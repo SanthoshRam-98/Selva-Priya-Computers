8c23aeaa3ee31af6e6591b5af7c98dfc8af3aed4
SanthoshRam-98 <santhoshram.rubydev@gmail.com>
2025-01-28 13:08:01 +0530

    Deleted and modified pages

--- selva-priya-computers/src/App.js
+++ selva-priya-computers/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Services from "./pages/Services";
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/services",
+    element: <Services />,
+  },
+]);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
